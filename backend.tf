067e56ee8cd06ea7bcb3124ade551465b89304dc
ravibhalala11795 <geekyrbhalala@gmail.com>
2024-02-28 20:58:28 -0500

    Updated dynamo db table name

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "myinfra2023terraformtraining"
     key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    region = "ca-central-1"
+    dynamodb_table = "myinfradynamodbtable"
   }
 }
